fec5e85c0095479e4622a53e5ddf1860e010cfab
Sarvendu   kr Pandey <88503595+esarvendu@users.noreply.github.com>
2023-04-02 19:03:03 +0530

    encapsulation assignment solution

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,20 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    // Constructor
-    public RWOnly(String name) {
-        this.name = name;
-    }
-
-    // Getter for name
     public String getName() {
         return name;
     }
 
-    // Setter for name
     public void setName(String name) {
         this.name = name;
     }
-
-
 }

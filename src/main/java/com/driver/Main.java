750bf54bbcf95ffedb3b7592c71a6a1074addf6e
Sarvendu   kr Pandey <88503595+esarvendu@users.noreply.github.com>
2023-04-02 18:54:03 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly("John");
+
+        // Use setter to set value to name
+        obj.setName("Doe");
+
+        // Use getter to print the value of name
+        System.out.println(obj.getName()); // This should print "Doe"
+    }
   
 }
